2a793259ceb4ff86ba204fb6e4960d7c0c072752
apounds329 <73663098+apounds329@users.noreply.github.com>
2021-03-26 19:29:20 -0400

    css
    clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -84,6 +84,7 @@ function leaveButton() { mouthOpen.reverse(); }
 // Ear wiggle
 // ----------
 
+
 const earWiggle = gsap.timeline({ paused: true, repeat: 2 });
 earWiggle.set('.ear-right', { transformOrigin:"center center" });
 earWiggle.to('.ear-right', {duration: 0.1, rotation: 45});
@@ -91,7 +92,7 @@ earWiggle.to('.ear-right', {duration: 0.1, rotation: 0});
 
 window.setInterval(() => earWiggle.play(0), 2500);
 
-// ------------
+// -------------
 // Eye tracking
 // -------------
 
